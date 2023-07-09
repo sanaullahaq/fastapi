f8799933130f984b0ec95b416cec4b1c3bc8b0ff
Sanaulla Haq <sanaullahaq01@gmail.com>
2023-07-09 17:59:41 +0600

    LC

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,25 +1,41 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from .. import database, schemas, models,utils
+from .. import database, schemas, models,utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+# def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
+    '''
+    This `OAuth2PasswordRequestForm` is fastapi's built in feature what asks user for credentials and
+    return user credentials in a dict object
+    {
+        'username': 'what user enters', 'password': 'what user enters'
+    }
+    '''
+    # user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
         )
     
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials"
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials"
         )
     
     # create token
-    # return token
+    access_token = oauth2.create_access_token(data={"user_email": user.email})
+    '''
+    this is only user choice what will be send to create_token, here we have only decided user_email,
+    we can also user_password, user_role
+    '''
 
-    return {"token": "example token"}
+    # return token
+    return {"access_token": access_token, "token_type": "bearer"}
--- app/utils.py
+++ app/utils.py
@@ -7,7 +7,6 @@ pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 def hash(password: str):
     return pwd_context.hash(password)
 
-
 '''
 Basically `pwd_context.verify(plain_password, hashed_password)` will convert the plain password into a hashed password and will match with hashed password sent and will return true/false
 '''
