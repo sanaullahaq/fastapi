c015dacea572b90dfc2ff251c1d9adddee4b9cad
Sanaulla Haq <sanaullahaq01@gmail.com>
2024-09-05 14:46:05 +0600

    lc

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from . import models
 from fastapi import FastAPI
 from . routers import posts, users, auth, votes
 from . config import settings
-
+from fastapi.middleware.cors import CORSMiddleware
 """
 At the start-up of the program we are commanding `sqlalchemy` to create all the tables,
 Since at the current stage of the program we are usig `alembic` database migrations tool,
@@ -15,6 +15,28 @@ As everything is already there.
 
 app = FastAPI()
 
+#https://fastapi.tiangolo.com/tutorial/cors/#use-corsmiddleware
+# origins = [
+#     "https://google.com",
+#     "https://youtube.com",
+# ]
+origins=["*"]
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"]
+)
+"""
+Basically we are allowing everybody(origins) to send API request to our server with `origins=["*"]`
+`allow_credentials=True` -> Credentials (Authorization headers, Cookies, etc).
+`allow_methods=["*"]` -> Specific HTTP methods (POST, PUT) or all of them with the wildcard "*".
+`allow_headers=["*"]` -> Specific HTTP headers or all of them with the wildcard "*".
+
+CORS - Cross-Origin Resource Sharing: comes handy when you server is made for private use, only
+request from specific origins will be allowed, only specific methods will be allowed. etc etc.
+"""
 
 app.include_router(posts.router)
 # this includes the path-operations lives in 'routers/posts.py' file using object 'router' of type 'APIRouter'
